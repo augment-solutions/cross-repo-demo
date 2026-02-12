3c8e5405283a00fb2dd098478e36e50d6df9fdf4
Robbert Kauffman <robbert@augmentcode.com>
2026-02-12 15:37:28 -0500

    added admin dashboard to docker compose scripts

--- docker-compose-down.sh
+++ docker-compose-down.sh
@@ -3,3 +3,4 @@ docker compose -f ../cross-repo-demo-product-service/docker-compose.yml down
 docker compose -f ../cross-repo-demo-inventory-service/docker-compose.yml down
 docker compose -f ../cross-repo-demo-catalog-service/docker-compose.yml down
 docker compose -f ../cross-repo-demo-storefront-web/docker-compose.yml down
+docker compose -f ../cross-repo-demo-admin-dashboard/docker-compose.yml down
